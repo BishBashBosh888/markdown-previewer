9291df95d221f89e95cb1fb1b6042ba09b536fac
BishBashBosh888 <fredericklauw@gmail.com>
2025-03-28 19:32:02 +1100

    forgot to add page title

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import MarkdownPreviewer from "./components/MarkdownPreviewer"
 import "./assets/styles/styles.css"
+import { useEffect } from "react"
 
 function App() {
-
+  useEffect(()=>{
+    document.title="Markdown Previewer"
+  })
+  
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-200">
       <MarkdownPreviewer/>
